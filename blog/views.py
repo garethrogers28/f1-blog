4383cd77abd028baba88f1b6381f426107a45d15
Gareth Rogers <Gareth.Rogers@creditsafeuk.com>
2026-04-23 11:19:26 +0100

    Complete set up. Scaffold Django project with Bootstrap and auth views

---
+++ blog/views.py
@@ -0,0 +1,19 @@
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import redirect, render
+
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Account created successfully. You can now log in.')
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
