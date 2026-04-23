0dca14175c7a7df2e4394ee74c69de43a071ce88
Gareth Rogers <Gareth.Rogers@creditsafeuk.com>
2026-04-23 12:01:34 +0100

    Add post model with admin registration

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    content = models.TextField()
+    excerpt = models.TextField(max_length=300, blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.title
