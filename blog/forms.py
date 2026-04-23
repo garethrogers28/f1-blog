25c5e12400218c4612941696255528a3701af923
Gareth Rogers <Gareth.Rogers@creditsafeuk.com>
2026-04-23 15:23:59 +0100

    Add comment display and form on post detail

--- blog/forms.py
+++ blog/forms.py
@@ -1,8 +1,15 @@
+from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import Comment
 
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('body',)        
--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,7 @@ from django.contrib import messages
 from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, redirect, render
 
-from .forms import CustomUserCreationForm
+from .forms import CommentForm, CustomUserCreationForm
 from .models import Post
 
 
@@ -16,7 +16,20 @@ def home(request):
 
 def post_detail(request, slug):
     post = get_object_or_404(Post, slug=slug)
-    return render(request, 'blog/post_detail.html', {'post': post})
+    comments = post.comments.all()
+    if request.method == 'POST' and request.user.is_authenticated:
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.author = request.user
+            comment.save()
+            return redirect('post_detail', slug=slug)
+    else:
+        form = CommentForm()
+    return render(request, 'blog/post_detail.html', {
+        'post': post, 'comments': comments, 'form': form,
+    })
 
 
 def register(request):
