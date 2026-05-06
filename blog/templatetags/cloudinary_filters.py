11a93b84a01fe02de57cbf94ac6a7defa0e31900
Gareth Rogers <Gareth.Rogers@creditsafeuk.com>
2026-05-06 14:06:29 +0100

    Add cloudinary_filters templatetag library

---
+++ blog/templatetags/cloudinary_filters.py
@@ -0,0 +1,22 @@
+from django import template
+
+register = template.Library()
+
+
+@register.filter
+def cloudinary_transform(url, options):
+    """
+    Add Cloudinary transformations to an image URL.
+    Usage: {{ post.featured_image.url|cloudinary_transform:"w_800,h_450,c_fill,q_auto,f_auto" }}
+    """
+    if not url:
+        return url
+    url = str(url)
+    # Ensure HTTPS
+    if url.startswith('http://'):
+        url = url.replace('http://', 'https://', 1)
+    # Cloudinary URLs look like: https://res.cloudinary.com/CLOUD/image/upload/v123/file.jpg
+    # We insert transformations after /upload/
+    if '/upload/' in url:
+        return url.replace('/upload/', f'/upload/{options}/')
+    return url
