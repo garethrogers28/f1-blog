8ce94357f8f655fdc0ce4b5b416e6bf08184e37f
Gareth Rogers <Gareth.Rogers@creditsafeuk.com>
2026-05-08 09:39:48 +0100

    Register driver, race, prediction, raceresult models to admin

--- predictions/admin.py
+++ predictions/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Driver, Race, Prediction, RaceResult
 
-# Register your models here.
+
+@admin.register(Driver)
+class DriverAdmin(admin.ModelAdmin):
+    list_display = ('name', 'team', 'number')
+    list_filter = ('team',)
+    search_fields = ('name',)
+
+
+@admin.register(Race)
+class RaceAdmin(admin.ModelAdmin):
+    list_display = ('name', 'circuit', 'date', 'prediction_deadline', 'is_completed')
+    prepopulated_fields = {'slug': ('name',)}
+    list_filter = ('is_completed',)
+
+
+@admin.register(Prediction)
+class PredictionAdmin(admin.ModelAdmin):
+    list_display = ('user', 'race', 'pole_driver', 'p1_driver', 'p2_driver', 'p3_driver')
+    list_filter = ('race',)
+
+
+@admin.register(RaceResult)
+class RaceResultAdmin(admin.ModelAdmin):
+    list_display = ('race', 'pole_driver', 'p1_driver', 'p2_driver', 'p3_driver')
