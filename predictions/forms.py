576884aaf06164b6318f33b71be2d9717e0f94ee
Gareth Rogers <Gareth.Rogers@creditsafeuk.com>
2026-05-11 12:44:55 +0100

    Update form to edit name, favourite driver/team

--- predictions/forms.py
+++ predictions/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Prediction, Driver
+from .models import Prediction, Driver, UserProfile
 
 
 class PredictionForm(forms.ModelForm):
@@ -19,3 +19,44 @@ class PredictionForm(forms.ModelForm):
         for field_name in self.fields:
             self.fields[field_name].queryset = drivers
             self.fields[field_name].widget.attrs['class'] = 'form-select'
+
+# List of F1 teams for the dropdown
+# First item is an empty option shown by default
+TEAM_CHOICES = [('', '--- Select a team ---')] + [
+    ('Aston Martin', 'Aston Martin'),
+    ('Alpine', 'Alpine'),
+    ('Audi', 'Audi'),
+    ('Cadillac', 'Cadillac'),
+    ('Ferrari', 'Ferrari'),
+    ('Haas', 'Haas'),
+    ('McLaren', 'McLaren'),
+    ('Mercedes', 'Mercedes'),
+    ('Racing Bulls', 'Racing Bulls'),
+    ('Red Bull', 'Red Bull'),
+    ('Williams', 'Williams'),
+]
+
+
+class UserProfileForm(forms.ModelForm):
+    # ModelForm automatically creates form fields from the UserProfile model
+    class Meta:
+        model = UserProfile
+        fields = ['display_name', 'favourite_team', 'favourite_driver']
+        labels = {
+            'display_name': 'Display Name',
+            'favourite_team': 'Favourite Team',
+            'favourite_driver': 'Favourite Driver',
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Override favourite_team to use a dropdown with predefined choices
+        # instead of a free-text input
+        self.fields['favourite_team'].widget = forms.Select(
+            choices=TEAM_CHOICES, attrs={'class': 'form-select'}
+        )
+        # Set the driver dropdown to show all drivers
+        self.fields['favourite_driver'].queryset = Driver.objects.all()
+        # Add Bootstrap styling to all fields
+        self.fields['favourite_driver'].widget.attrs['class'] = 'form-select'
+        self.fields['display_name'].widget.attrs['class'] = 'form-control'
