597a01324bab051bb2971ea0722a6cc2c2fa3ead
Gareth Rogers <Gareth.Rogers@creditsafeuk.com>
2026-05-29 13:00:56 +0100

    Replace HttpResponseForbidden with redirect in delete_comment and edit_comment

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,6 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
-from django.http import HttpResponseForbidden # delete once removed. 
 from django.shortcuts import get_object_or_404, redirect, render
 
 from .forms import CommentForm, CustomUserCreationForm
@@ -56,7 +55,7 @@ def edit_comment(request, pk):
     """
     comment = get_object_or_404(Comment, pk=pk)
     if comment.author != request.user:
-        return HttpResponseForbidden() #fix 
+        return redirect('post_detail', slug=comment.post.slug) #fix 
     if request.method == 'POST':
         form = CommentForm(request.POST, instance=comment)
         if form.is_valid():
@@ -78,7 +77,7 @@ def delete_comment(request, pk):
     """
     comment = get_object_or_404(Comment, pk=pk)
     if comment.author != request.user:
-        return HttpResponseForbidden() # fix 
+        return redirect('post_detail', slug=comment.post.slug)
     if request.method == 'POST':
         slug = comment.post.slug
         comment.delete()
