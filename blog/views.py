8aff80546a50e626f9da726a8b04e0a02fdfd0b4
Gareth Rogers <Gareth.Rogers@creditsafeuk.com>
2026-05-29 13:21:38 +0100

    Add favicon (chequered flag)

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,6 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, redirect, render
-
 from .forms import CommentForm, CustomUserCreationForm
 from .models import Comment, Post
 
