39558748dcbe321fad506b059c91babb9156a19e
Gareth Rogers <Gareth.Rogers@creditsafeuk.com>
2026-06-08 12:05:23 +0100

    Add Bootstrap form-control class to login and register forms

--- blog/forms.py
+++ blog/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from .models import Comment
 
@@ -12,6 +12,20 @@ class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs['class'] = 'form-control'
+
+class CustomLoginForm(AuthenticationForm):
+    """
+    Login form with Bootstrap form-control class on inputs.
+    """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs['class'] = 'form-control'
 
 
 class CommentForm(forms.ModelForm):
--- f1blog/urls.py
+++ f1blog/urls.py
@@ -19,13 +19,15 @@ from django.contrib.auth import views as auth_views
 from django.urls import include, path
 
 from blog import views as blog_views
+from blog.forms import CustomLoginForm
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path(
         'login/',
         auth_views.LoginView.as_view(
-            template_name='registration/login.html'
+            template_name='registration/login.html',
+            authentication_form=CustomLoginForm,
         ),
         name='login',
     ),
